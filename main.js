34482c26811bd33f4701bd7f761d9b8491486388
ramoncastro674 <tadeuramoncastro@gmail.com>
2024-04-16 14:34:29 -0300

    primeiro commit

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const form = document.getElementById('form-contatos');
-//const imgAprovado = `<img src="./images/aprovado.png" alt="Emoji celebrando"/>`
-//const imgReprovado = `<img src="./images/reprovado.png" alt="Emoji decepcionado" />`
+const form = document.getElementById('form-tarefas');
 const contatos = [];
-const numeros = [];
-//const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-//const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-//const notaMinima = parseFloat(prompt("Digite a nota mínima:"))
-
 let linhas = '';
 
 form.addEventListener('submit', function(e) {
@@ -17,25 +10,21 @@ form.addEventListener('submit', function(e) {
 });
 
 function adicionaLinha() {
-    const inputContato = document.getElementById('nomeContato')
-    const inputNumero = document.getElementById('Numero')
+    const inputContato = document.getElementById('nome')
 
     if(contatos.includes(inputContato.value)) {
         alert(`O contato: ${inputContato.value} já foi inserido`);
     } else {        
             contatos.push(inputContato.value);
-            numeros.push(parseFloat(inputNumero.value));
-        
+
             let linha = '<tr>';
             linha += `<td>${inputContato.value}</td>`;
-            linha += `<td>${inputNumero.value}</td>`;
             linha += '</tr>';
         
             linhas += linha;
     }
 
     inputContato.value = '';
-    inputNumero.value = '';
 }   
 
 function atualizaTabela() {
@@ -43,15 +32,13 @@ function atualizaTabela() {
     corpoTabela.innerHTML = linhas;
 }
 
-const handlePhone = (event) => {
-    let input = event.target
-    input.value = phoneMask(input.value)
-  }
-  
-  const phoneMask = (value) => {
-    if (!value) return ""
-    value = value.replace(/\D/g,'')
-    value = value.replace(/(\d{2})(\d)/,"($1) $2")
-    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
-    return value
-  }
+$('#inserir').click(() => {
+    const valor = $('#nome').focus().val()
+    if (valor) {
+        $('#nome').val('')
+        $('<li>')
+        .text(valor)
+        .appendTo('#lista')
+        .click(() => $(event.target).toggleClass('riscado')) 
+        }    
+    }) 
